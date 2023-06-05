c4198d41168f98185bd36497dc25502bcb57635d
FominIgor <113089549+FominIgor@users.noreply.github.com>
2023-06-05 21:35:34 +0300

    Add files via upload

---
+++ 4 photos 1 word/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace FourPhotosOneWordGame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 598);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(441, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(146, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 539);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(901, 721);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 4 photos 1 word/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FourPhotosOneWordGame
+{
+    public partial class MainForm : Form
+    {
+        private List<string[]> imageAndAnswerList;
+        private int currentIndex;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // Инициализация списка перед загрузкой данных
+            imageAndAnswerList = new List<string[]>();
+
+            // Загрузка данных из файла
+            string[] lines = File.ReadAllLines("answers.txt");
+
+            // Разделение строк на изображение и ответ
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('|');
+                string image = parts[0].Trim();
+                string answer = parts[1].Trim();
+                imageAndAnswerList.Add(new string[] { image, answer });
+            }
+
+            // Отображение первого изображения
+            if (imageAndAnswerList.Count > 0)
+            {
+                currentIndex = 0;
+                string[] firstImageAndAnswer = imageAndAnswerList[currentIndex];
+                pictureBox1.Image = Image.FromFile(firstImageAndAnswer[0]);
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string[] currentImageAndAnswer = imageAndAnswerList[currentIndex];
+            string correctAnswer = currentImageAndAnswer[1];
+            string userAnswer = textBox1.Text.Trim();
+
+            if (userAnswer.Length == correctAnswer.Length)
+            {
+                if (userAnswer.ToLower() == correctAnswer.ToLower())
+                {
+                    MessageBox.Show("Правильный ответ!");
+                    NextImage();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный ответ!");
+                }
+
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+
+
+        private void NextImage()
+        {
+            currentIndex++;
+            if (currentIndex < imageAndAnswerList.Count)
+            {
+                string[] nextImageAndAnswer = imageAndAnswerList[currentIndex];
+                pictureBox1.Image = Image.FromFile(nextImageAndAnswer[0]);
+
+            }
+            else
+            {
+                MessageBox.Show("Вы прошли все уровни!");
+                // Дополнительные действия при завершении игры
+            }
+        }
+
+    }
+}
